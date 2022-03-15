8f1a35fc0c3cd2a184074599aaf6510db5be4bbe
Sravan Kairamkonda <35738653+SravanGameDev@users.noreply.github.com>
2022-03-15 23:35:28 +0000

    Added terrain animation

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -9,17 +9,19 @@ public class MeshGenerator : MonoBehaviour
 {
     Mesh mesh;
 
-    [SerializeField] private int xSize = 30;
-    [SerializeField] private int zSize = 30;
+    [SerializeField] private int xSize = 50;
+    [SerializeField] private int zSize = 50;
 
     Vector3[] vertices;
     int[] triangles;
     Color[] colors;
 
-    float minTerrainHeight;
-    float maxTerrainHeight;
-
-    private float scale=0.3f;
+    [SerializeField] private float amplitude1=1;
+    [SerializeField] private float frequency1 =1;
+    [SerializeField] private float amplitude2=0.5f;
+    [SerializeField] private float frequency2 =1f;
+    [SerializeField] private float amplitude3=0.1f;
+    [SerializeField] private float frequency3=5f;
 
     public Gradient gradient;
 
@@ -30,10 +32,8 @@ public class MeshGenerator : MonoBehaviour
         this.GetComponent<MeshFilter>().mesh = mesh;
         mesh.name = "Terrain";
 
-        //this.GetComponent<MeshRenderer>().material.color = Color.white;
 
         StartCoroutine(CreateShape());
-        //CreateShape();
     }
 
     private void Update()
@@ -50,12 +50,8 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = GetNoise(x, z);
+                float y = CalculateNoise(x, z);
                 vertices[i] = new Vector3(x, y, z);
-
-                if (y > maxTerrainHeight) maxTerrainHeight = y;
-                if (y < minTerrainHeight) minTerrainHeight = y;
-                
 
                 i++;
             }
@@ -81,21 +77,10 @@ public class MeshGenerator : MonoBehaviour
                 vert++;
                 tris += 6;
 
-                yield return new WaitForSeconds(0.001f);
+                yield return new WaitForSeconds(0.0001f);
             }
 
             vert++;
-        }
-
-        colors = new Color[vertices.Length];
-
-        for (int i=0, z = 0; z <= zSize; z++)
-        {
-            for (int x = 0; x <= xSize; x++)
-            {
-                float height = Mathf.InverseLerp(minTerrainHeight,maxTerrainHeight, vertices[i].y);
-                colors[i] = gradient.Evaluate(height);
-            }
         }
     }
 
@@ -110,10 +95,13 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
-    public float GetNoise(int x, int z)
+    private float CalculateNoise(float x, float z)
     {
-        float y = Mathf.PerlinNoise(x * scale, z * scale) * 2f;
-
-        return y;
+        float noise;
+        noise = Mathf.PerlinNoise(x, z) * 5;
+        noise += Mathf.PerlinNoise(x * amplitude1, z * amplitude1) * frequency1;
+        noise -= Mathf.PerlinNoise(x * amplitude2, z * amplitude2) * frequency2;
+        noise += Mathf.PerlinNoise(x * amplitude3, z * amplitude3) * frequency3 * 2;
+        return noise;
     }
 }
